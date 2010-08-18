67f14f33cb4337271304a47d8806440a935ed145
Alexander Stante <stante@gmail.com>
2010-08-18 18:20:30 +0000

    Support for loading of submeshes added
    All submeshes are imported as objects into Blender.

--- import_shape_m3.py
+++ import_shape_m3.py
@@ -24,8 +24,7 @@ import bpy
 
 from bpy.props import *
 from struct import unpack_from, calcsize
-
-
+from os.path import basename
 
 # M3 File representation encapsulating file handle
 class M3File:
@@ -63,8 +62,12 @@ class M3File:
 		(string, ) = unpack_from("<" + str(count) + "s", self.file.read(calcsize("<" + str(count) + "s")))
 		return string
 		
+	def readId(self):
+		id = self.readString(4)
+		return id[::-1]
+		
 	def readM3Reference(self):
-		return M3Reference.read(self)
+		return M3Reference(self)
 		
 	def readIndices(self, reference):
 		faces = []
@@ -86,114 +89,57 @@ class M3File:
 		self.file.seek(offset)
 		
 		for i in range(count):
-			regions.append(M3Region.read(self))
+			regions.append(M3Region(self))
 			
 		return regions
 
 class M3Reference:
-	def __init__(self):
-		self.Count = 0
-		self.Index = 0
-		self.Flags = 0
+	def __init__(self, file):
+		self.Count = file.readUnsignedInt()
+		self.Index = file.readUnsignedInt()
+		self.Flags = file.readUnsignedInt()
 		
-	def read(file):
-		m3reference = M3Reference()
-		
-		m3reference.Count = file.readUnsignedInt()
-		m3reference.Index = file.readUnsignedInt()
-		m3reference.Flags = file.readUnsignedInt()
-		
-		return m3reference
-
-
 class M3Region:
-	def __init__(self):
-		self.D1        = 0
-		self.OffsetVert   = 0
-		self.NumVert   = 0
-		self.OffsetFaces  = 0
-		self.NumFaces  = 0
-		self.BoneCount = 0
-		self.IndBone   = 0
-		self.Numbone   = 0
-		self.S1        = []
-		
-	def read(file):
-		m3region = M3Region()
-		
-		m3region.D1        = file.readUnsignedInt()
-		m3region.D2        = file.readUnsignedInt()
-		m3region.OffsetVert   = file.readUnsignedInt()
-		m3region.NumVert   = file.readUnsignedInt()
-		m3region.OffsetFaces  = file.readUnsignedInt()
-		m3region.NumFaces  = file.readUnsignedInt()
-		m3region.BoneCount = file.readUnsignedShort()
-		m3region.IndBone   = file.readUnsignedShort()
-		m3region.NumBone   = file.readUnsignedShort()
-		m3region.s1        = file.readArrayUnsignedShort(3)
-		#file.skipBytes(6)
-		
-		print("M3Region-----------------")
-		print("D1:            " + str(m3region.D1))
-		print("OffsetVert:    " + str(m3region.OffsetVert))
-		print("NumVert:       " + str(m3region.NumVert))
-		print("OffsetFaces:   " + str(m3region.OffsetFaces))
-		print("NumFaces:      " + str(m3region.NumFaces))
-		print("BoneCount:     " + str(m3region.BoneCount))
-		print("IndBone:       " + str(m3region.IndBone))
-		print("NumBone:       " + str(m3region.NumBone))
-		print("------------------------")
-		
-		return m3region
 
+	def __init__(self, file):
+		self.D1           = file.readUnsignedInt()
+		self.D2           = file.readUnsignedInt()
+		self.OffsetVert   = file.readUnsignedInt()
+		self.NumVert      = file.readUnsignedInt()
+		self.OffsetFaces  = file.readUnsignedInt()
+		self.NumFaces     = file.readUnsignedInt()
+		self.BoneCount    = file.readUnsignedShort()
+		self.IndBone      = file.readUnsignedShort()
+		self.NumBone      = file.readUnsignedShort()
+		self.s1           = file.readArrayUnsignedShort(3)
+
+# TODO read bat and msec		
 class M3Div:
-	def __init__(self):
-		self.Indices = []
-		self.Regions = []
+
+	def __init__(self, file):
 		self.Bat     = []
 		self.Msec    = []
-	
-	def read(file):
-		div = M3Div()
-		referenceIndices = file.readM3Reference()
-		referenceRegions = file.readM3Reference()
-		referenceBat     = file.readM3Reference()
-		referenceMsec    = file.readM3Reference()
-		
-		print("Model Divisions")
-		print("===============")
-		print("Faces:  " + str(referenceIndices.Count))
-		print("Meshes: " + str(referenceRegions.Count))
+		referenceIndices = M3Reference(file)
+		referenceRegions = M3Reference(file)
+		referenceBat     = M3Reference(file)
+		referenceMsec    = M3Reference(file)
 		
 		referenceIndicesEntry = file.ReferenceTable[referenceIndices.Index]
 		referenceRegionsEntry = file.ReferenceTable[referenceRegions.Index]
 		
-		referenceIndicesEntry.print()
-		referenceRegionsEntry.print()
-			
-		div.Indices = file.readIndices(referenceIndicesEntry)
-		div.Regions = file.readRegions(referenceRegionsEntry)
+		self.Indices = file.readIndices(referenceIndicesEntry)
+		self.Regions = file.readRegions(referenceRegionsEntry)
 
-		return div
-
-# TODO: Fix read using M3File
 class M3Vertex:
 	
-	def __init__(self):
-		self.Position = (0, 0, 0)
-		
-	def read(file):
-		m3vertex = M3Vertex()
-		m3vertex.Position   = file.readVertex()
-		m3vertex.BoneWeight = file.readBytes(4)
-		m3vertex.BoneIndex  = file.readBytes(4)
-		m3vertex.Normal     = file.readBytes(4)
-		m3vertex.UV         = file.readArrayUnsignedShort(2)
-		m3vertex.D1         = file.readUnsignedInt()
-		m3vertex.Tangent    = file.readBytes(4)
-		
-		return m3vertex
-		
+	def __init__(self, file):
+		self.Position   = file.readVertex()
+		self.BoneWeight = file.readBytes(4)
+		self.BoneIndex  = file.readBytes(4)
+		self.Normal     = file.readBytes(4)
+		self.UV         = file.readArrayUnsignedShort(2)
+		self.D1         = file.readUnsignedInt()
+		self.Tangent    = file.readBytes(4)		
 
 class M3Model23:
 	
@@ -207,140 +153,98 @@ class M3Model23:
 		file.skipBytes(0x60)
 		m3model.Flags = file.readUnsignedInt()
 
-		vertexReference = M3Reference.read(file)
-		viewReference   = M3Reference.read(file)
-		
-		print("Flags:  " + hex(m3model.Flags))
+		vertexReference = M3Reference(file)
+		viewReference   = M3Reference(file)
 		
 		viewReferenceEntry = file.ReferenceTable[viewReference.Index]
 		vertexReferenceEntry = file.ReferenceTable[vertexReference.Index]
 		
-		viewReferenceEntry.print()
-		vertexReferenceEntry.print()
-		
 		if ((m3model.Flags & 0x20000) != 0):
 			if ((m3model.Flags & 0x40000) != 0):
 				count = vertexReferenceEntry.Count // 36
-				print(count)
 				file.seek(vertexReferenceEntry.Offset)
 				for i in range(count):
-					ver = M3Vertex.read(file)
+					ver = M3Vertex(file)
 					m3model.Vertices.append(ver.Position)
-					# print(self.Vertices[i].Position)
-		
-		#print("DIV: " + hex(self.DivDataOffset))
-		#print("DIV: " + hex(self.DivDataCount))
 			
 		file.seek(viewReferenceEntry.Offset)
-		div = M3Div.read(file)
+		div = M3Div(file)
 		
-		#for i in range(len(div.Regions)):
-		i = 1
-
-		add = div.Regions[i].OffsetVert
+		submeshes = []
 		
-		print("div.Indices length:")
-		print(len(div.Indices))
-		
-		for j in range(div.Regions[i].OffsetFaces, div.Regions[i].OffsetFaces + div.Regions[i].NumFaces, 3):
-			m3model.Faces.append((div.Indices[j]+add, div.Indices[j+1]+add, div.Indices[j+2]+add))
+		for i in range(len(div.Regions)):
+			offset = div.Regions[i].OffsetVert
+			number = div.Regions[i].NumVert
 			
-		#for i in range(22, len(div.Indices)-3, 3):
-		#	m3model.Faces.append((div.Indices[i], div.Indices[i+1], div.Indices[i+2]))
+			vertices = m3model.Vertices[offset:offset + number]
+			faces = []
+			
+			for j in range(div.Regions[i].OffsetFaces, div.Regions[i].OffsetFaces + div.Regions[i].NumFaces, 3):
+				faces.append((div.Indices[j], div.Indices[j+1], div.Indices[j+2]))
+				
+			submeshes.append(Submesh(vertices, faces))
 		
-		return m3model
-		
-		#print(self.Vertices)
+		return submeshes
 					
 class M3ReferenceEntry:
 	
-	def __init__(self):
-		self.Id = []
-		self.Offset = 0
-		self.Count = 0
-		self.Type = 0
-
-	def print(self):
-		print("============================")
-		print("Id:     " + str(self.Id))
-		print("Offset: " + hex(self.Offset))
-		print("Count:  " + str(self.Count))
-		print("Type:   " + hex(self.Type))
-		print("============================")
-		
-	def read(file):
-		m3referenceEntry = M3ReferenceEntry()
-		
-		m3referenceEntry.Id = file.readString(4)
-		m3referenceEntry.Id = m3referenceEntry.Id[::-1]
-		m3referenceEntry.Offset = file.readUnsignedInt()
-		m3referenceEntry.Count = file.readUnsignedInt()
-		m3referenceEntry.Type = file.readUnsignedInt()
-		
-		return m3referenceEntry
-	
+	def __init__(self, file):
+		self.Id     = file.readId()
+		self.Offset = file.readUnsignedInt()
+		self.Count  = file.readUnsignedInt()
+		self.Type   = file.readUnsignedInt()
 
 class M3Header:
 
-	def __init__(self):
-		self.Id = []
-		self.ReferenceTableOffset = 0
-		self.ReferenceTableCount = 0
-		self.ModelCount = 0
-		self.ModelIndex = 0
+	def __init__(self, file):
+		self.Id                   = file.readId()
+		self.ReferenceTableOffset = file.readUnsignedInt()
+		self.ReferenceTableCount  = file.readUnsignedInt()
+		self.ModelCount           = file.readUnsignedInt()
+		self.ModelIndex           = file.readUnsignedInt()
 		
-	def read(file):
-		m3header = M3Header()
-	
-		m3header.Id = file.readString(4)
-		m3header.ReferenceTableOffset = file.readUnsignedInt()
-		m3header.ReferenceTableCount = file.readUnsignedInt()
-		m3header.ModelCount = file.readUnsignedInt()
-		m3header.ModelIndex = file.readUnsignedInt()
-		
-		print("M3 File Header")
-		print("--------------")
-		print(m3header.Id)
-		print(hex(m3header.ReferenceTableOffset))
-		print(m3header.ReferenceTableCount)
-		print(m3header.ModelCount)
-		print(m3header.ModelIndex)
-		print("--------------")
-
-		count  = m3header.ReferenceTableCount
-		offset = m3header.ReferenceTableOffset
+		count  = self.ReferenceTableCount
+		offset = self.ReferenceTableOffset
 		
 		file.seek(offset)
 		
 		for i in range(count):
-			file.ReferenceTable.append(M3ReferenceEntry.read(file))
-			
-		return m3header
-				
-
+			file.ReferenceTable.append(M3ReferenceEntry(file))
+		
 class M3Data:
+
 	def __init__(self, filepath):
 		file = M3File(filepath)
 		
 		# Reading file header
-		self.m3Header = M3Header.read(file)
+		self.m3Header = M3Header(file)
 		
 		# Reading model
 		modelReference = file.ReferenceTable[self.m3Header.ModelIndex]
-		modelReference.print()
 		
 		file.seek(modelReference.Offset)
 		self.m3Model = M3Model23.read(file)
+		
+class Submesh:
 
-def import_m3(filepath):
+	def __init__(self, vertices, faces):
+		self.Name = "NONAME"
+		self.Vertices = vertices
+		self.Faces = faces
+
+def import_m3(context, filepath):
 	m3data = M3Data(filepath)
-	mesh = bpy.data.meshes.new("MyMesh")
-	mesh.from_pydata(m3data.m3Model.Vertices, [], m3data.m3Model.Faces)
-	mesh.update(True)
-	ob = bpy.data.objects.new("MyObject", mesh)
-	sc = bpy.data.scenes[0].objects.link(ob)
+	name = basename(filepath)
+	
+	for i in range(len(m3data.m3Model)):
+		mesh = bpy.data.meshes.new(name)
+		mesh.from_pydata(m3data.m3Model[i].Vertices, [], m3data.m3Model[i].Faces)
+		mesh.update(True)
+		ob = bpy.data.objects.new(name, mesh)
+		context.scene.objects.link(ob)
 
 class IMPORT_OT_m3(bpy.types.Operator):
+	'''Import from Blizzard M3 file'''
 	bl_idname = "import_shape.m3"
 	bl_label  = "Import M3"
 
@@ -355,7 +259,7 @@ class IMPORT_OT_m3(bpy.types.Operator):
 		return True
 		
 	def execute(self, context):
-		import_m3(self.properties.filepath)
+		import_m3(context, self.properties.filepath)
 		return {'FINISHED'}
 		
 	def invoke(self, context, event):
