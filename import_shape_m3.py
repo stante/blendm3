cd50f8f748399846b116b00425d3386cf56e4dbf
Alexander Stante <stante@gmail.com>
2010-08-21 12:48:50 +0000

    Starting with adding material / texture support

--- import_shape_m3.py
+++ import_shape_m3.py
@@ -21,6 +21,7 @@
 # This script imports the M3 file into Blender for editing
 
 import bpy
+import os
 
 from bpy.props import *
 from struct import unpack_from, calcsize
@@ -54,6 +55,9 @@ class M3File:
         
     def readArrayUnsignedShort(self, count):
         return unpack_from("<" + str(count) + "H", self.file.read(calcsize("<" + str(count) + "H")))
+        
+    def readArraySignedShort(self, count):
+        return unpack_from("<" + str(count) + "h", self.file.read(calcsize("<" + str(count) + "h")))
         
     def readVertex(self):
         return unpack_from("<3f", self.file.read(calcsize("<3f")))
@@ -148,7 +152,20 @@ class M3Vertex:
 
         # Vertex type specifies how many UV entries the Vertex format contains
         for i in range(type + 1):
-            self.UV.append(file.readArrayUnsignedShort(2))
+            (u, v) = file.readArraySignedShort(2)
+            u = u / 2048.0
+            v = v / 2048.0
+            
+            if (u > 1.0) or (u < 0.0):
+                print("WTFu : " + str(u))
+            
+            if (v > 1.0) or (v < 0.0):
+                print("WTFv : " + str(v))
+
+            
+            u = 0
+            v = 1 - v
+            self.UV.append((u,v))
         
         # Further investigation of this flag needed
         if ((flags & 0x200) != 0):
@@ -161,6 +178,7 @@ class M3Model23:
     def __init__(self):
         self.Flags = 0
         self.Vertices = []
+        self.UV1 = []
         self.Faces = []
         
     def read(file):
@@ -171,6 +189,8 @@ class M3Model23:
         vertexReference = file.readReferenceEntry()
         viewReference   = file.readReferenceEntry()
         
+        uvIndex = 0
+        
         if ((m3model.Flags & 0x100000) != 0):
             count = vertexReference.Count // 44
             print("Reading %s vertices, Format: 0x100000, Flags: %s" % (count, hex(m3model.Flags)))
@@ -178,6 +198,7 @@ class M3Model23:
             for i in range(count):
                 ver = M3Vertex(file, M3Vertex.VERTEX44, m3model.Flags)
                 m3model.Vertices.append(ver.Position)
+                m3model.UV1.append(ver.UV[uvIndex])
                 
         elif ((m3model.Flags & 0x80000) != 0):
             count = vertexReference.Count // 40
@@ -186,7 +207,8 @@ class M3Model23:
             for i in range(count):
                 ver = M3Vertex(file, M3Vertex.VERTEX40, m3model.Flags)
                 m3model.Vertices.append(ver.Position)
- 
+                m3model.UV1.append(ver.UV[uvIndex])
+
         elif ((m3model.Flags & 0x40000) != 0):
             count = vertexReference.Count // 36
             print("Reading %s vertices, Format: 0x40000, Flags: %s" % (count, hex(m3model.Flags)))
@@ -194,6 +216,7 @@ class M3Model23:
             for i in range(count):
                 ver = M3Vertex(file, M3Vertex.VERTEX36, m3model.Flags)
                 m3model.Vertices.append(ver.Position)
+                m3model.UV1.append(ver.UV[uvIndex])
 
         elif ((m3model.Flags & 0x20000) != 0):
             count = vertexReference.Count // 32
@@ -202,7 +225,8 @@ class M3Model23:
             for i in range(count):
                 ver = M3Vertex(file, M3Vertex.VERTEX32, m3model.Flags)
                 m3model.Vertices.append(ver.Position)
-
+                m3model.UV1.append(ver.UV[uvIndex])
+                
         else:
             raise Exception('import_m3: !ERROR! Unsupported vertex format. Flags: %s' % hex(m3model.Flags))
         
@@ -216,12 +240,14 @@ class M3Model23:
             number = regn.NumVert
             
             vertices = m3model.Vertices[offset:offset + number]
+            uvs = m3model.UV1
             faces = []
             
             for j in range(regn.OffsetFaces, regn.OffsetFaces + regn.NumFaces, 3):
                 faces.append((div.Indices[j], div.Indices[j+1], div.Indices[j+2]))
+                #uvs.append((m3model.UV1[div.Indices[j]], m3model.UV1[div.Indices[j+1]], m3model.UV1[div.Indices[j+1]]))
                 
-            submeshes.append(Submesh(vertices, faces))
+            submeshes.append(Submesh(vertices, faces, uvs))
         
         return submeshes
                     
@@ -277,13 +303,31 @@ class M3Data:
         
         file.seek(modelReference.Offset)
         self.m3Model = M3Model23.read(file)
+        
+def createMaterial():
+    # Create image texture from image. Change here if the snippet
+    # folder is not located in you home directory.
+    realpath = os.path.expanduser('H:/Downloads/Work/Assets/Textures/armory_diffuse.dds')
+    tex = bpy.data.textures.new('ColorTex')
+    tex.type = 'IMAGE'
+    tex = tex.recast_type()
+    tex.image = bpy.data.images.load(realpath)
+    tex.use_alpha = True
+    # Create shadeless material and MTex
+    mat = bpy.data.materials.new('TexMat')
+    mat.shadeless = True
+    mat.add_texture(texture = tex,
+    texture_coordinates = 'UV',
+    map_to = 'COLOR')
+    return mat
                 
 class Submesh:
 
-    def __init__(self, vertices, faces):
+    def __init__(self, vertices, faces, uv1):
         self.Name = "NONAME"
         self.Vertices = vertices
         self.Faces = faces
+        self.UV1 = uv1
 
 def import_m3(context, filepath):
     m3data = M3Data(filepath)
@@ -292,8 +336,29 @@ def import_m3(context, filepath):
     for submesh in m3data.m3Model:
         mesh = bpy.data.meshes.new(name)
         mesh.from_pydata(submesh.Vertices, [], submesh.Faces)
+        
+        mesh.add_uv_texture()
+        uvtex = mesh.uv_textures[0]
+        uvtex.name = "foo"
+        
+#        for i, texcoord in enumerate(submesh.UV1):
+#            data = uvtex.data[i]
+#            data.uv1 = texcoord[0]
+#            data.uv2 = texcoord[1]
+#            data.uv3 = texcoord[2]
+
+        for i, face in enumerate(submesh.Faces):
+            data = uvtex.data[i]
+            data.uv1 = submesh.UV1[face[0]]
+            data.uv2 = submesh.UV1[face[1]]
+            data.uv3 = submesh.UV1[face[2]]
+            data.uv4 = (0,0)
+            
+        m = createMaterial()
+        
         mesh.update(True)
         ob = bpy.data.objects.new(name, mesh)
+        ob.data.add_material(m)
         context.scene.objects.link(ob)
 
 class IMPORT_OT_m3(bpy.types.Operator):
